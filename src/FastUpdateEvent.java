52fe56b64c67e52402d3b1630947ee9b7f2646c1
Nand Kishore <nandck@gmail.com>
2015-12-07 22:40:40 -0800

    Add TCP FAST Class.

---
+++ src/FastUpdateEvent.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FastUpdateEvent extends Event{
+	Flow flow;
+	double timeInterval = 0.1;
+	
+	public FastUpdateEvent(double startTime, double timeInterval, Flow flow){
+		super(startTime);
+		this.flow = flow;
+		this.timeInterval = timeInterval;
+	}
+
+	@Override
+	public List<Event> handle() {
+		ArrayList<Event> newEvents = new ArrayList<Event>();
+		flow.windowSize = flow.minRtt/flow.rtt*flow.windowSize + 1;
+		newEvents.add(new FastUpdateEvent(this.time+timeInterval, timeInterval, flow));
+		return newEvents;
+	}
+	
+	public String toString() {
+		return super.toString()
+				+ "\t\t\tEvent Type: FastUpdateEvent\t\t\tDetails: Updating Window with TCP FAST";
+	}
+}
